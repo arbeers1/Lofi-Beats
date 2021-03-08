4b1b59bbd7fa0e33c1de436f7415d30823101ba4
ALEXANDER BEERS <arbeers@wisc.edu>
2021-03-07 20:03:31 -0600

    Added outline for Rules

--- Lofi Beats/MainActivity.cs
+++ Lofi Beats/MainActivity.cs
@@ -1,10 +1,14 @@
 ﻿using Android.App;
 using Android.OS;
+using Android.Widget;
 using Android.Runtime;
 using AndroidX.AppCompat.App;
 
 namespace Lofi_Beats
 {
+    /// <summary>
+    /// Entry Point for program
+    /// </summary>
     [Activity(Label = "@string/app_name", Theme = "@style/Theme.AppCompat.NoActionBar", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
@@ -14,6 +18,25 @@ namespace Lofi_Beats
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            Rules.GenerateRules();
+
+            //Event Handler for Play Button
+            bool playButtonStatus = true;
+            ImageButton playBtn = FindViewById<ImageButton>(Resource.Id.playButton);
+            playBtn.Click += (sender, e) =>
+            {
+                if (playButtonStatus)
+                {
+                    playBtn.SetImageResource(Android.Resource.Drawable.IcMediaPlay);
+                    playButtonStatus = !playButtonStatus;
+                }
+                else
+                {
+                    playBtn.SetImageResource(Android.Resource.Drawable.IcMediaPause);
+                    playButtonStatus = !playButtonStatus;
+                }
+            };
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
---
+++ Lofi Beats/Rules.cs
@@ -0,0 +1,48 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lofi_Beats
+{
+    /// <summary>
+    /// Defines the logic of the music composition algorithm. Any given music node must follow a specific path as defined by rules.
+    /// Example:
+    /// 
+    /// If rules are defined by:
+    /// 1 -> 2, 3
+    /// 2 -> 1
+    /// 3 -> 2
+    /// 
+    /// Then the algorithm follows:
+    /// Iteration 1: 1
+    /// Iteration 2: 2, 3
+    /// Iteration 3: 1, 2
+    /// Iteration 4: 2, 3, 1
+    /// </summary>
+    public static class Rules
+    {
+        //Defines the rule set for track 1
+        public static int[][] RuleSet1 { get; private set; }
+
+        //Defines the rule set for track 2
+        public static int[][] RuleSet2 { get; private set; }
+
+        public static void GenerateRules()
+        {
+   
+        }
+
+        public static int[] LookUp(int target)
+        {
+            return null;
+        }
+
+    }
+}
