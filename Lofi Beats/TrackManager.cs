fbd88951e6356a54ce9f26154112cd944f9127cc
ALEXANDER BEERS <arbeers@wisc.edu>
2021-03-07 21:05:02 -0600

    Outlined TrackManager

---
+++ Lofi Beats/TrackManager.cs
@@ -0,0 +1,30 @@
+﻿
+namespace Lofi_Beats
+{
+    /// <summary>
+    /// Manages multiple tracks
+    /// </summary>
+    public class TrackManager
+    {
+        //Tracks - Each play a single audio file at a time
+        private Track Track1;
+
+        private Track Track2;
+
+        /// <summary>
+        /// Play Audio
+        /// </summary>
+        public void play()
+        {
+
+        }
+
+        /// <summary>
+        /// Pause Audio
+        /// </summary>
+        public void pause()
+        {
+
+        }
+    }
+}
