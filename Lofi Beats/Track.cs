8af63209993f45967613fb4c1f9881e9e9b4800c
ALEXANDER BEERS <arbeers@wisc.edu>
2021-03-07 21:14:00 -0600

    Outline Track

---
+++ Lofi Beats/Track.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Lofi_Beats
+{
+    public class Track
+    {
+        //Queue for Audio
+        private Queue queue;
+
+        private Queue nextQueue;
+
+        public Track()
+        {
+            queue = new Queue();
+            nextQueue = new Queue();
+        }
+    }
+}
